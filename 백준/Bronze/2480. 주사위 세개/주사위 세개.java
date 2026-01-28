cd58ef85f00762511c0277b69370dfb30405cbd9
mac0616 <76460285+mac0616@users.noreply.github.com>
2026-01-28 19:37:31 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a, b, c;
+		a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+		
+        if (a==b && b==c){
+            System.out.println(10000+(a)*1000);
+        } else if (a==b || a==c){
+            System.out.println(1000+(a)*100);
+        } else if (c==b){
+            System.out.println(1000+(c)*100);
+        } else if (a!= b && b!=c && a!=c){
+            int max;
+            if (a>b){
+                if(c>a){
+                    max=c;
+                } else {
+                    max=a;
+                }
+            }else {
+                if(c>b){
+                    max=c;
+                } else{
+                    max=b;
+                }
+            }
+            System.out.println(max*100);
+        }
+    }
+}
