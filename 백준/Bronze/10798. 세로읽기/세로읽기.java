9e80b8945ac041dcd668b215a8fe685690a30561
mac0616 <76460285+mac0616@users.noreply.github.com>
2026-02-12 23:34:00 +0900

    [Bronze I] Title: 세로읽기, Time: 168 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char[][] ch = new char[5][15];
+        int max = 0;
+        
+        for(int i=0; i<ch.length; i++){
+            String str = sc.next();
+            if(max < str.length()) max = str.length();
+            
+            for(int j = 0; j< str.length(); j++){
+                ch[i][j] = str.charAt(j);
+            }
+        }
+        for(int i=0; i<max; i++){
+            for(int j=0; j<5; j++){
+                if(ch[j][i] == '\0') continue;
+                System.out.print(ch[j][i]);
+            }
+        }
+    }
+}
