cfd297394f30ca59d0ec6dc26fd3e468800fa343
mac0616 <76460285+mac0616@users.noreply.github.com>
2026-03-04 17:57:24 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length+1];
+        
+        int last1 = num_list[num_list.length - 1]; 
+        int last2 = num_list[num_list.length - 2];
+
+        for(int i=0; i<num_list.length; i++){
+            answer[i] = num_list[i];
+        }
+        if (last1 > last2) {
+            answer[num_list.length] = last1 - last2;
+        } else {
+            answer[num_list.length] = last1 * 2;
+        }
+        
+        return answer;
+    }
+}
